541e001d6639d3e1545a9c69971c54166c46ace8
ylfyt <yalfayat@gmail.com>
2024-05-22 00:16:49 +0700

    change pwsh profile(2)

--- pwsh/profile.ps1
+++ pwsh/profile.ps1
@@ -1,4 +1,7 @@
+Clear-Host
 $PSStyle.FileInfo.Directory = ""
+
+oh-my-posh init pwsh --config $HOME\dotfiles\pwsh\mytheme.omp.json | Invoke-Expression
 
 # Fzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
@@ -13,3 +16,22 @@ Set-PSReadLineOption -Colors @{ Operator = 'Red' }
 Set-Alias j z
 Set-Alias l ls
 Set-Alias g git
+
+function which {
+    param(
+        [string]$CommandName
+    )
+    $commandInfo = Get-Command -Name $CommandName -ErrorAction SilentlyContinue
+    if ($commandInfo.CommandType -eq "Alias") {
+        return $commandInfo.DisplayName
+    }
+    if ($commandInfo.Path -ne $null) {
+        return $commandInfo.Path
+    }
+    $commandPath = Get-Command $CommandName -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Definition
+    if ($commandPath -ne $null) {
+        return $commandPath
+    }
+    return $null
+}
+
