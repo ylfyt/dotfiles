dbef185bac9016387af09d7d50acf7006de18bb7
ylfyt <yalfayat@gmail.com>
2024-10-02 11:41:35 +0700

    adding timeout

--- plasma-command-outputs.sh
+++ plasma-command-outputs.sh
@@ -5,4 +5,4 @@
 free | awk '/Mem/{printf("M:%02d\n", $3/$2 * 100)}'
 
 # print ping latency
-(ping -c 1 -w 400 google.com 2> /dev/null | grep 'time=' || echo '0=0=0=0') | awk -F'=' '{print $4}' | awk '{if ($1 > 500) print "😡"; else if ($1 > 0) print $1; else print "❌"}'
+(timeout 2s ping -c 1 -w 400 8.8.8.8 2> /dev/null | grep 'time=' || echo '0=0=0=0') | awk -F'=' '{print $4}' | awk '{if ($1 > 500) print "😡"; else if ($1 > 0) print $1; else print "❌"}'
